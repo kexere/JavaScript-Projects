fdafef1802315be5c4c24f67cc04a3672d2dbfbd
kexere <126893696+kexere@users.noreply.github.com>
2023-08-02 13:28:37 +0100

    Added Assignment

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,11 @@
+var B = 'Hello, World!'; //defining variable and giving it a string value
+window.alert(B); //calling variable B
+var C = "\"This is a quote\" <br>"; //defining variable and giving it a string value
+document.write(C); //caliing variable C
+document.write('Let\'s concatenate'
++ ' this <br>');
+var D = 'Assign a concatenated string value to a variable' + ' and display it in the browser. <br>'; //defining variable and giving it a oncatenated string value
+document.write(D); //calling variable D
+var cat='Tom', mouse='Jerry', dog='Spike'; //defining variables and giving them a string values
+document.write(cat + ' and ' + mouse + '<br>'); //calling variable cat and mouse
+document.write(10 + 5); //sum both numbers
