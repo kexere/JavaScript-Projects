095c01d2ffa1fc1d118748277ea873a319816a98
kexere <126893696+kexere@users.noreply.github.com>
2023-08-26 17:01:14 +0100

    Added Function Challenge

---
+++ Basic JavaScript Projects/Function_Challenge/JS/main.js
@@ -0,0 +1,6 @@
+document.getElementById("red").style.color = "red";
+
+function myFunction() {
+    var x = document.getElementById("myText").value;
+    document.getElementById("greeting").innerHTML = "Hello " + x + "! Have a great day!";
+}
